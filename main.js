74966bde7b3e5d024f45ab8ab43fe6fc3bd4ede7
“JarlM06” <jarl.ebbesen@outlook.com>
2023-10-16 10:16:18 +0200

    La til en oppgraderings-knapp som øker antall pops man får for hvert klikk

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
 // Angir ulike variabler
 let totalPops = 0;
+let popsMultiplier = 1;
+let multiplyPopsCost = 20;
 
 // Funksjonen når 'pops' skal legges til
 function addPop() {
     // Henter tekstelementet som viser antall pops
     let popCounter = document.getElementById("popCounter");
     // Øker antall pops med 1
-    totalPops += 1;
+    totalPops += popsMultiplier;
     // Endrer teksten til å stemme med antall pops brukeren har
     popCounter.textContent = `${totalPops} pops`;
-}
+};
+
+function multiplyPops() {
+    let popCounter = document.getElementById("popCounter");
+    let multiplyPopsButton = document.getElementById("multiplyPopsButton");
+    if(totalPops >= multiplyPopsCost) {
+        totalPops -= multiplyPopsCost;
+        popsMultiplier += 1;
+        multiplyPopsCost *= 2;
+        multiplyPopsButton.textContent = `+1 pop per click \n Cost: ${multiplyPopsCost} pops \n Current pops per click: ${popsMultiplier}`;
+        popCounter.textContent = `${totalPops} pops`;
+    }
+};

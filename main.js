a240e1f49f8bfb0e962d8403f8b1f0486146f5ca
“JarlM06” <jarl.ebbesen@outlook.com>
2023-10-16 09:42:20 +0200

    Du kan nå få poeng ved å klikke på ballongen

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-function test() {
-    console.log('test yes');
+// Angir ulike variabler
+let totalPops = 0;
+
+// Funksjonen når 'pops' skal legges til
+function addPop() {
+    // Henter tekstelementet som viser antall pops
+    let popCounter = document.getElementById("popCounter");
+    // Øker antall pops med 1
+    totalPops += 1;
+    // Endrer teksten til å stemme med antall pops brukeren har
+    popCounter.textContent = `${totalPops} pops`;
 }
